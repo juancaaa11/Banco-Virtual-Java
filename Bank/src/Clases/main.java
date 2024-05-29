39312e3d81b3e6ec8a123992a75ee7fee9d19ff6
Juanca <juanca.jcglp26@gmail.com>
2024-05-29 02:53:45 +0200

    New

--- Bank/src/Clases/main.java
+++ Bank/src/Clases/main.java
@@ -9,6 +9,9 @@
 
 
 package Clases;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
 /*
 *Inicializa el Frame de Bienvenido.
 */
@@ -20,21 +23,20 @@ public class main {
 
 	public static void main(String[] args) {
 		
-		JFrame bank = new JFrame("Bank!"); 
+		 JFrame bank = new JFrame("Bank!");
 
-        // Configurar el JFrame
-        bank.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-        bank.setSize(718, 590); 
-        bank.setLocationRelativeTo(null); // Centrar el JFrame en la pantalla
+	        // Configurar el JFrame
+	        bank.setIconImage(new ImageIcon(".//src/Imagenes/logo.png").getImage()); // Ruta relativa corregida
+	        bank.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        bank.setSize(718, 590);
+	        bank.setLocationRelativeTo(null); // Centrar el JFrame en la pantalla
 
-        
-        bienvenido panelBienvenido = new bienvenido();
+	        // Asegúrate de que la clase bienvenido está correctamente definida
+	        bienvenido panelBienvenido = new bienvenido();
 
-       
-        bank.getContentPane().add(panelBienvenido);
+	        bank.getContentPane().add(panelBienvenido);
 
-        
-        bank.setVisible(true);
+	        bank.setVisible(true);
         
         
         
--- README.md
+++
@@ -1,6 +0,0 @@
-# PROYECTO JAVA BANCO VIRTUAL
-
-# SUPPORT FOR ME PLEASE
-
-It would be appreciated if you helped me and leave your likes and share with your friends, thank you very much.
-
